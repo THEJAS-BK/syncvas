4219dab84f6964235e4de85417dae117992d1021
THEJAS-BK <thejasbk998@gmail.com>
2026-03-25 14:46:00 +0000

    reworking on sockets

--- backend/src/controllers/socketManger.ts
+++ backend/src/controllers/socketManger.ts
@@ -1,7 +1,7 @@
-import { time } from "node:console";
+
 import { Server } from "socket.io";
 
-let connections: { [key: string]: string[] } = {};
+let connections: { [key: string]: string[]|any } = {};
 let messages: { [key: string]: any } = {};
 let timeOnline: { [key: string]: Date } = {};
 
@@ -12,39 +12,7 @@ const setSocketConnection = (server: any) => {
       credentials: true,
     },
   });
-  io.on("connection", (socket) => {
-    socket.on("join", (path) => {
-      if (!connections[path]) {
-        connections[path] = [];
-      }
-      connections[path].push(socket.id);
-      timeOnline[socket.id]=new Date();
-
-      connections[path].forEach(ele=>{
-        io.to(ele).emit("user-joined",socket.id,connections[path])
-      })
-
-      if(messages[path]!==undefined){
-        for(let a=0;a<messages[path].length;a++){
-          io.to(socket.id).emit("chat-message",messages[path][a]['data'],
-            messages[path][a]['sender'],messages[path][a]['socket-id-sender']
-          )
-        }
-      }
-
-      console.log(connections[path]);
-    });
-
-    socket.on("signal", (toId, message) => {
-      io.to(toId).emit("signal", message);
-    });
-
-    socket.on("chat-message", (data, sender) => {
-      
-    });
-
-    socket.on("disconnect", () => {});
-  });
+ 
   return io;
 };
 
