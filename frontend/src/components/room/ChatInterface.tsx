eed74e56d703d04a38261593624046b727046bfd
THEJAS-BK <thejasbk998@gmail.com>
2026-03-26 15:57:53 +0000

    updates

--- frontend/src/components/room/ChatInterface.tsx
+++ frontend/src/components/room/ChatInterface.tsx
@@ -1,7 +1,12 @@
 export default function ChatInterface() {
     return(
-        <div className="bg-blue-300 flex-1 min-h-screen " >
-            hello
+        <div className="border flex-1 pb-4 min-h-screen flex flex-col justify-between" >
+            <div className="bg-blue-200 flex-1">
+
+            </div>
+            <div>
+                <input type="text" placeholder="text somthing" />
+            </div>
         </div>
     )
 };
