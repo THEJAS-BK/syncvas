cff78a2b1bfa53b732d1b4929708f3d89daf497b
THEJAS-BK <thejasbk998@gmail.com>
2026-03-27 14:15:54 +0000

    progress on chat interface

--- frontend/src/components/room/ChatBox.tsx
+++ frontend/src/components/room/ChatBox.tsx
@@ -1,7 +1,14 @@
-export default function ChatBox() {
+import type { Message } from "../../types/Chat"
+type ChatBoxProp={
+    message:Message,
+    isOwn:boolean
+}
+export default function ChatBox({message,isOwn}:ChatBoxProp) {
     return(
-        <div className="self-end">
-            helgsfgsflo
+        <div className={isOwn?"self-end":"self-start"}>
+            {message.text}
+            <span>  :   </span>
+            <span>{message.sender}</span>
         </div>
     )
 };
--- frontend/src/components/room/ChatInterface.tsx
+++ frontend/src/components/room/ChatInterface.tsx
@@ -2,9 +2,10 @@ import ChatBox from "./ChatBox";
 import Button from "@mui/material/Button";
 import { socket } from "../../services/socket";
 import { useEffect, useState } from "react";
-
+import type { Message } from "../../types/Chat";
 export default function ChatInterface() {
-  const [message, setMessage] = useState("");
+  const [inputText, setInputText] = useState<string>("");
+  const [messages, setMessages] = useState<Message[]>([]);
   useEffect(() => {
     socket.connect();
 
@@ -12,23 +13,43 @@ export default function ChatInterface() {
       console.log("Connected :", socket.id);
     });
 
-    socket.on("message", (data) => {
-      console.log("Message received from backend:", data);
+    socket.on("receive_message", (data: Message) => {
+     setMessages((prev) => [...prev, data]);
     });
 
-  });
-  const handleMesSend = () => {
-    socket.emit("message",message);
+     return () => {
+    socket.off("receive_message");
+    socket.off("connect");
+  };
+  },[]);
+  const handleMesSend = ():void => {
+
+    if(!inputText.trim())return;
+
+    const newMessage:Message={
+      text:inputText,
+      sender:socket.id||"unknown"
+    }
+    socket.emit("send_message", newMessage);
+    setMessages((prev)=>[...prev,newMessage]);
+    setInputText("")
   };
   return (
     <div className="border flex-1 pb-4 min-h-screen flex flex-col justify-between">
       <div className="bg-blue-200 flex-1 px-2 py-6 flex flex-col">
-        <ChatBox />
+       {messages.map((mes,idx)=>{
+         return(
+          <ChatBox
+          key={idx}
+          message={mes}
+          isOwn={mes.sender === socket.id}
+      />   )
+       })}
       </div>
       <div className="px-2 py-4">
         <input
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
           type="text"
           placeholder="text something"
         />
