a6b5cd81a0752fb5166467e7acce22e98eba1b5f
THEJAS-BK <thejasbk998@gmail.com>
2026-05-24 09:05:12 -0300

    canvas draw feature

--- frontend/src/components/room/MultiCursor.tsx
+++ frontend/src/components/room/MultiCursor.tsx
@@ -1,7 +1,62 @@
+import { useEffect, useRef, useState } from "react";
+
 export default function MultiCursor() {
-  return (
-    <div className="w-[70%] flex flex-col">
-      Hello
-    </div>
-  )
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const isDrawing = useRef(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
+
+    const startDrawing = (e: MouseEvent) => {
+      isDrawing.current = true;
+      ctx.beginPath();
+      const rect = canvas.getBoundingClientRect();
+
+      const scaleX = canvas.width / rect.width;
+      const scaleY = canvas.height / rect.height;
+
+      const x = (e.clientX - rect.left) * scaleX;
+      const y = (e.clientY - rect.top) * scaleY;
+      ctx.moveTo(x, y);
+    };
+
+    const draw = (e: MouseEvent) => {
+      if (!isDrawing.current) return;
+      const rect = canvas.getBoundingClientRect();
+
+      const scaleX = canvas.width / rect.width;
+      const scaleY = canvas.height / rect.height;
+
+      const x = (e.clientX - rect.left) * scaleX;
+      const y = (e.clientY - rect.top) * scaleY;
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    };
+
+    const stopDrawing = () => {
+      isDrawing.current = false;
+      ctx.closePath();
+    };
+
+    canvas.addEventListener("mousedown", startDrawing);
+    canvas.addEventListener("mousemove", draw);
+    window.addEventListener("mouseup", stopDrawing);
+
+    return () => {
+      canvas.removeEventListener("mousedown", startDrawing);
+      canvas.removeEventListener("mousemove", draw);
+      window.removeEventListener("mouseup", stopDrawing);
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} width={900} height={600}></canvas>;
 }
