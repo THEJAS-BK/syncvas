af99ae8ca5661345084798c65e7e08ffde92feb2
THEJAS-BK <thejasbk998@gmail.com>
2026-06-15 05:33:40 -0300

    frontend ui updates

--- frontend/src/components/room/RoomNavbar.tsx
+++ frontend/src/components/room/RoomNavbar.tsx
@@ -1,89 +1,12 @@
-import { useParams } from "react-router-dom";
-import { socket } from "../../services/socket";
 
-type BoardImage = {
-  image: string;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-};
-
-interface RoomNavProp {
-  setCursorDash: (value: boolean) => void;
-  cursorDash: boolean;
-  floatChatInterface: boolean;
-  setFloatChatInterface: (value: boolean) => void;
-  images: React.RefObject<BoardImage[]>;
-  setRedrawVersion: React.Dispatch<React.SetStateAction<number>>;
-  eraserMode:boolean;
-  setEraserMode:  React.Dispatch<React.SetStateAction<boolean>>;
-  eraserRef: React.MutableRefObject<boolean>;
-}
-
-export default function RoomNavbar({
-  setCursorDash,
-  cursorDash,
-  images,
-  floatChatInterface,
-  setFloatChatInterface,
-  setRedrawVersion,
-
-  eraserMode,
-  setEraserMode,
-  eraserRef
-}: RoomNavProp) {
-  const { roomId } = useParams();
-  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (!file) return;
-
-    const reader = new FileReader();
-
-    reader.onload = () => {
-      const base64 = reader.result as string;
-      const id = crypto.randomUUID();
-
-      const imageData = {
-        id,
-        image: base64,
-        x: 100,
-        y: 100,
-        width: 400,
-        height: 300,
-      };
-
-      images.current?.push(imageData);
-      setRedrawVersion((v) => v + 1);
-
-      socket.emit("image-upload", { roomId, ...imageData });
-    };
-
-    reader.readAsDataURL(file);
-  };
+export default function RoomNavbar(
+) {
+  
   return (
     <>
       <div className="bg-blue-200 h-16 flex justify-between align-middle">
-        <button onClick={() => {
-          const newValue = !eraserMode;
-          setEraserMode(newValue);
-          eraserRef.current = newValue;
-        }}>
-          {eraserMode ? " pen" : " Eraser"}
-        </button>
-
-        <div>
-          <button onClick={() => setCursorDash(!cursorDash)}>
-            open multiBoard
-          </button>
-
-          <input type="file" accept="image/*" onChange={handleImageUpload} />
-        </div>
-        <div>
-          <button onClick={() => setFloatChatInterface(!floatChatInterface)}>
-            Float
-          </button>
-        </div>
+       Eh;;jklgjdfgdf
+ 
       </div>
     </>
   );
--- frontend/src/pages/RoomPage.tsx
+++ frontend/src/pages/RoomPage.tsx
@@ -6,6 +6,11 @@ import { useEffect, useRef, useState } from "react";
 import { WebRtcProvider } from "../context/WebRtcContext";
 import MultiCursor from "../components/room/Multicursor/MultiCursor";
 
+//lucide react components
+import { Pencil, TableOfContents, TypeOutline } from "lucide-react";
+import { Image, Eraser, MousePointer, Hand } from "lucide-react";
+import { socket } from "../services/socket";
+
 type BoardImage = {
   id: string; // add this
   image: string;
@@ -22,51 +27,132 @@ export default function RoomPage() {
   const [floatChatInterface, setFloatChatInterface] = useState(false);
   const images = useRef<BoardImage[]>([]);
   const [eraserMode, setEraserMode] = useState(false);
-  const eraserRef=useRef(false);
+  const eraserRef = useRef(false);
   //redraw
   const [redrawVersion, setRedrawVersion] = useState(0);
 
   if (!roomId) return <div>Not found</div>;
 
+  //hambergerMenu
+  const [isHambergerMenuOpen, setIsHambergerMenuOpen] = useState(false);
 
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
 
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const base64 = reader.result as string;
+      const id = crypto.randomUUID();
+
+      const imageData = {
+        id,
+        image: base64,
+        x: 100,
+        y: 100,
+        width: 400,
+        height: 300,
+        rotation: 0,
+      };
+
+      images.current?.push(imageData);
+      setRedrawVersion((v) => v + 1);
+
+      socket.emit("image-upload", { roomId, ...imageData });
+    };
+
+    reader.readAsDataURL(file);
+  };
   return (
-    <WebRtcProvider roomId={roomId}>
-      <div className="h-screen flex flex-col">
-        <RoomNavbar
-          floatChatInterface={floatChatInterface}
-          setFloatChatInterface={setFloatChatInterface}
-          setCursorDash={setOpenCursor}
-          cursorDash={openCursor}
-          images={images}
-          setRedrawVersion={setRedrawVersion}
-          eraserMode={eraserMode}
-          setEraserMode={setEraserMode}
-          eraserRef={eraserRef}
-        />
-        <main className="flex-1 flex ">
-          {/* cursor interface not open*/}
-          {!openCursor && (
-            <MainContent
+    <>
+      {/*image upload*/}
+      <input
+        type="file"
+        id="image-upload"
+        className="hidden"
+        accept="image/*"
+        onChange={handleImageUpload}
+      />
+
+      <WebRtcProvider roomId={roomId}>
+        <div className="h-screen flex flex-col">
+          {!openCursor && <RoomNavbar />}
+          <main className="flex-1 flex static">
+            {openCursor && (
+              <button
+                onClick={() => setIsHambergerMenuOpen(!isHambergerMenuOpen)}
+                className="absolute text-white z-20"
+              >
+                <TableOfContents />
+              </button>
+            )}
+
+            {isHambergerMenuOpen && (
+              <div className="absolute top-16 left-0 bg-white shadow-lg z-20">
+                <ul className="py-2">
+                  <li className="px-4 py-2 hover:bg-gray-200">Option 1</li>
+                  <li className="px-4 py-2 hover:bg-gray-200">Option 2</li>
+                  <li className="px-4 py-2 hover:bg-gray-200">Option 3</li>
+                </ul>
+              </div>
+            )}
+            {/*center tools menu*/}
+            <div className="absolute top-10 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white shadow-lg z-20">
+              <div className="flex gap-4 shadow-lg">
+                <span>
+                  <MousePointer />
+                </span>
+                <span>
+                  <Pencil />
+                </span>
+                <span>
+                  <TypeOutline />
+                </span>
+                <span>
+                  <Hand />
+                </span>
+                <span>
+                  <Eraser />
+                </span>
+                <span>
+                  <label htmlFor="image-upload">
+                    <Image />
+                  </label>
+                </span>
+                <button onClick={() => setOpenCursor(!openCursor)}>
+                  VideoConf
+                </button>
+                <button
+                  onClick={() => setFloatChatInterface(!floatChatInterface)}
+                >
+                  Float
+                </button>
+              </div>
+            </div>
+            {/* cursor interface not open*/}
+            {!openCursor && (
+              <MainContent
+                floatChatInterface={floatChatInterface}
+                roomId={roomId}
+              />
+            )}
+            {openCursor && (
+              <MultiCursor
+                imageUpdate={redrawVersion}
+                eraserRef={eraserRef}
+                images={images}
+                floatChatInterface={floatChatInterface}
+              />
+            )}
+            <ChatInterface
               floatChatInterface={floatChatInterface}
+              cursorDash={openCursor}
               roomId={roomId}
             />
-          )}
-          {openCursor && (
-            <MultiCursor
-              imageUpdate={redrawVersion}
-              eraserRef={eraserRef}
-              images={images}
-              floatChatInterface={floatChatInterface}
-            />
-          )}
-          <ChatInterface
-            floatChatInterface={floatChatInterface}
-            cursorDash={openCursor}
-            roomId={roomId}
-          />
-        </main>
-      </div>
-    </WebRtcProvider>
+          </main>
+        </div>
+      </WebRtcProvider>
+    </>
   );
 }
